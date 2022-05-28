795dc724d08e8caaef2da17b4a5becbe29c3bd31
Md-Mehedi <1705082@ugrad.cse.buet.ac.bd>
2022-05-28 17:24:25 +0600

    Server client add to use Encryption and Decryption

--- Client.py
+++ Client.py
@@ -1,12 +1,127 @@
-import socket            
- 
-s = socket.socket()        
- 
-port = 12345               
- 
-s.connect(('127.0.0.1', port))
- 
-# receive data from the server and decoding to get the string.
-print (s.recv(1024).decode())
-# close the connection
-s.close()   
+######################################################################
+##----------------------------- Imports ----------------------------##
+##------------------------------------------------------------------##
+
+import os
+import socket
+import sys
+
+from AES import AES
+from RSA import RSA
+
+##------------------------------------------------------------------##
+##----------------------------- Imports ----------------------------##
+######################################################################
+
+
+""" This is Receiver: BOB """
+
+
+######################################################################
+##------------------------ Global Definition -----------------------##
+##------------------------------------------------------------------##
+
+BUFFER_SIZE = 4096
+PORT = 12346
+IP = '127.0.0.1'
+
+##------------------------------------------------------------------##
+##------------------------ Global Definition -----------------------##
+######################################################################
+
+
+
+
+######################################################################
+##------------------------- Helper Functions -----------------------##
+##------------------------------------------------------------------##
+
+def send_ack(socket):
+  """ Send acknowledgement. It is called after every transmission """
+  socket.send("ACK".encode())
+
+def recv(socket):
+  """ Receive data chunk by chunk. \n
+  At first it receive the length of data and send an acknowledgement.
+  Afterthat it iterate until whole data received. And finally sent an acknowledgement """
+  data = ''
+  length = int(socket.recv(20).decode())  # Receiving length of data
+  send_ack(socket)                        # Sending acknowledgement
+  collect_byte = 0
+  while True:
+    if length - collect_byte <= BUFFER_SIZE:
+      fragment = socket.recv(length-collect_byte).decode()
+      data += fragment
+      break;
+    else :
+      fragment = socket.recv(BUFFER_SIZE).decode()
+      data += fragment
+      collect_byte += BUFFER_SIZE
+  send_ack(socket)
+  return data
+
+##------------------------------------------------------------------##
+##------------------------- Helper Functions -----------------------##
+######################################################################
+
+
+
+if __name__ == '__main__':
+  # Connection establishing with server
+  s = socket.socket()
+  s.connect((IP, PORT))
+
+  # Receiving encrypted text, encrypted key and public key
+  encrypted_text = recv(s)
+  print("========== Information About Decryption ==========")
+  print("Encrypted text : ", encrypted_text)
+  encrypted_key = recv(s)
+  print("Encrypted key  : ", encrypted_key)
+  e = int(recv(s))
+  n = int(recv(s))
+  print("          e, n : ", e, n)
+
+  # Initializing file and folder path
+  dirname = os.path.dirname(__file__)
+  secret_folder_path = dirname + "\\Don't open this"
+  private_key_file_path = secret_folder_path + "\\private_key.txt"
+  decrypted_file_path = secret_folder_path + "\\decrypted_text.txt"
+  
+  # If secret folder doesn't exist, create one
+  if os.path.exists(secret_folder_path) == False :
+    os.mkdir(secret_folder_path)
+
+  # Checking if secret file is exist. If doesn't exist terminate the execution
+  if os.path.exists(private_key_file_path) == False:
+    print("Secret file not found")
+    sys.exit()
+
+  # Reading private key from secret file
+  file = open(private_key_file_path, 'r')
+  line = file.readline()
+  d, n = line.split(" ")
+  file.close()
+  print("          d, n : ", d, n)
+  
+  # Decrypting
+  rsa = RSA(16)
+  rsa.set_private_key(int(d), int(n))
+  decrypted_key = rsa.decrypt(encrypted_key)
+  aes = AES(decrypted_key)
+  decrypted_text = aes.decrypt(encrypted_text)
+  print("    Plain text : ", decrypted_text)
+  print("           Key : ", decrypted_key)
+  print("========== Information About Decryption ==========")
+  print()
+
+  # Writing decrypted text to a file to match by the sender
+  file = open(decrypted_file_path, 'w+')
+  file.write(str.strip(decrypted_text))
+  file.close()
+  send_ack(s)
+
+  print("Write complete to file for Alice confirmation")
+
+
+
+  s.close()
--- Server.py
+++ Server.py
@@ -1,26 +1,134 @@
+######################################################################
+##----------------------------- Imports ----------------------------##
+##------------------------------------------------------------------##
 
+import os
 import socket
 
+from AES import AES
+from RSA import RSA
 
-s = socket.socket()        
-print ("Socket successfully created")
- 
+##------------------------------------------------------------------##
+##----------------------------- Imports ----------------------------##
+######################################################################
 
-port = 12345               
- 
-s.bind(('', port))        
-print ("socket binded to %s" %(port))
- 
-s.listen(5)    
-print ("socket is listening")           
- 
-while True:
- 
-# Establish connection with client.
-  c, addr = s.accept()    
+""" This is Sender: ALICE """
+
+
+######################################################################
+##------------------------ Global Definition -----------------------##
+##------------------------------------------------------------------##
+
+BUFFER_SIZE = 4096
+PORT = 12346
+
+##------------------------------------------------------------------##
+##------------------------ Global Definition -----------------------##
+######################################################################
+
+
+
+
+######################################################################
+##------------------------- Helper Functions -----------------------##
+##------------------------------------------------------------------##
+
+def recv_ack(socket):
+  """ Received acknowledgement as "ACK". If text mismatch return False otherwise True """
+  if socket.recv(4).decode() != 'ACK': 
+    print("Acknowledge not found")
+    return False
+  return True
+
+def send(socket, msg):
+  """ Send anything converting to bytes as chunk by chunk.\n
+  At first send the length of the message and wait for ACKNOWLEDGE
+  Message send as chunk by chunk size of BUFFER_SIZE. Finally wait for ACKNOWLEDGE """
+  msg = str(msg)
+  sent_byte = 0
+  socket.send(str(len(msg)).encode())   # Sending msg length
+  if not recv_ack(socket): return       # Check for acknowledgement
+  while True:
+    if len(msg) - sent_byte <= BUFFER_SIZE :
+      socket.send(msg[sent_byte:].encode())
+      break
+    else :
+      socket.send(msg[sent_byte:sent_byte+BUFFER_SIZE].encode())
+      sent_byte += BUFFER_SIZE
+  if not recv_ack(socket): return
+
+##------------------------------------------------------------------##
+##------------------------- Helper Functions -----------------------##
+######################################################################
+
+if __name__ == '__main__':
+  # Server connection establishing
+  server = socket.socket()        
+  print ("Socket successfully created")
+  server.bind(('', PORT))        
+  print ("socket binded to %s" %(PORT))
+  server.listen(5)    
+  print ("socket is listening")
+  client, addr = server.accept()    
   print ('Got connection from', addr )
- 
-  c.send('Thank you for connecting'.encode())
- 
-  c.close()
+  print()
 
+  # Take input of text and AES, RSA initialization
+  PLAIN_TEXT = "Offline r valo lage na"
+  KEY = "Val lage na"
+  aes = AES(KEY)
+  rsa = RSA(16)
+
+  # Encrypting
+  encrypted_text = aes.encrypt(PLAIN_TEXT)
+  encrypted_key = rsa.encrypt(KEY)
+  e, n = rsa.get_public_key()
+  d, n = rsa.get_private_key()
+
+  # Initializing file and folder path
+  dirname = os.path.dirname(__file__)
+  secret_folder_path = dirname + "\\Don't open this"
+  secret_file_path = secret_folder_path + "\\private_key.txt"
+  decrypted_file_path = secret_folder_path + "\\decrypted_text.txt"
+
+  # If secret folder doesn't exist, create one
+  if os.path.exists(secret_folder_path) == False :
+    os.mkdir(secret_folder_path)
+
+  # Writing private key in a secret file
+  file = open(secret_file_path, 'w+')
+  file.write(str(d) + " " + str(n))
+  file.close()
+
+  print("========== Information About Encryption ==========")
+  print("    Plain text : ", PLAIN_TEXT)
+  print("           Key : ", KEY)
+  print("Encrypted text : ", encrypted_text)
+  print(" Encrypted key : ", encrypted_key)
+  print("          e, n : ", e, n)
+  print("          d, n : ", d, n)
+  print("========== Information About Encryption ==========")
+  print()
+
+  # Sending encrypted text, encrypted key and public key
+  send(client, encrypted_text)
+  send(client, encrypted_key)
+  send(client, e)
+  send(client, n)
+
+  # Checking if decrypted text in client end is correct
+  print("========== Waiting for Bob's decryption... ==========")
+  if recv_ack(client) :
+    file = open(decrypted_file_path, "r")
+    decrypted_text:str = file.readline()
+    file.close()
+    print("    Plain text : ", PLAIN_TEXT)
+    print("Decrypted text : ", decrypted_text)
+    if decrypted_text == PLAIN_TEXT :
+      print("Encryption is ok")
+    else : 
+      print("Encryption don't ok")
+
+  # Closing server and client
+  client.close()
+  server.close()
